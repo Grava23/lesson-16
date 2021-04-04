80748e9e58327740b3bcfa0fabe3aca1f5ec9929
lesson-15 <niksanteg139@gmail.com>
2021-04-04 15:58:19 +0400

    window adedd

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var hotelSlider = new Swiper(".hotel-slider", {
+$(document).ready(function() {
+  var hotelSlider = new Swiper(".hotel-slider", {
     // Optional parameters
     loop: true, 
     // Navigation arrows
@@ -25,4 +26,25 @@ menuButton.addEventListener("click", function ( ) {
   document
   .querySelector(".navbar-bottom")
   .classList.toggle("navbar-bottom--visible");
+});
+
+ var modealButton = $("[data-toggle=modal]");
+ var closeModalButton = $(".modal__close");
+ modealButton.on('click', openModal);
+ closeModalButton.on("click", closeModal);
+
+function openModal() {
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.addClass('modal__overlay--visible');
+  modalDialog.addClass('modal__dialog--visible');
+}
+
+function closeModal(event) {
+  event.preventDefault();
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.removeClass('modal__overlay--visible');
+  modalDialog.removeClass('modal__dialog--visible');
+}
 });
